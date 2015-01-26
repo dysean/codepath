22141fe81b62f001e21b5dc07d9a548fb01a9914
Sean Dy <seand@seand-mba2.local>
2015-01-26 14:49:12 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Sean Dy on 1/26/15.
+//  Copyright (c) 2015 Sean Dy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
